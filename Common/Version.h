8792f9add76a6c2e4209ab84dd04d4bd3dded515
Martin Dubois <mdubois@kms-quebec.com>
2021-03-25 16:22:50 -0400

    1.0.4_Firmware1

--- Common/Version.h
+++ Common/Version.h
@@ -11,11 +11,11 @@
 
 #define VERSION_MAJOR         (1)
 #define VERSION_MINOR         (0)
-#define VERSION_BUILD         (3)
+#define VERSION_BUILD         (4)
 #define VERSION_COMPATIBILITY (0)
 
-#define VERSION_RC   1,0,3,0
-#define VERSION_STR  "1.0.3.0"
-#define VERSION_STR0 "1.0.3.0\0"
+#define VERSION_RC   1,0,4,0
+#define VERSION_STR  "1.0.4.0"
+#define VERSION_STR0 "1.0.4.0\0"
 
 #define VERSION_TYPE ""
