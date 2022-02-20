7bb96bcffa025991b65bfdec20ceeff8276033dd
Martin Dubois <mdubois@kms-quebec.com>
2022-02-20 10:15:19 -0500

    1.1.8_Darwin_i386

--- Common/Version.h
+++ Common/Version.h
@@ -11,11 +11,11 @@
 
 #define VERSION_MAJOR         (1)
 #define VERSION_MINOR         (1)
-#define VERSION_BUILD         (7)
+#define VERSION_BUILD         (8)
 #define VERSION_COMPATIBILITY (0)
 
-#define VERSION_RC   1,1,7,0
-#define VERSION_STR  "1.1.7.0"
-#define VERSION_STR0 "1.1.7.0\0"
+#define VERSION_RC   1,1,8,0
+#define VERSION_STR  "1.1.8.0"
+#define VERSION_STR0 "1.1.8.0\0"
 
 #define VERSION_TYPE ""
